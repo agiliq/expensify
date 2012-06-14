c8a33d4e22cb3a3dd834c0f17e0926abefc97b93
bitaVersion <pure.vintage.skt@gmail.com>
2012-06-14 15:15:56 +0530

    Wrote the initial db structure

--- exapp/models.py
+++ exapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class EventDetails(models.Model):
+    
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    venue = models.CharField(max_length=500)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+    def __unicode__(self):
+        return self.name
+    
+
+class ExpenseDetails(models.Model):
+    
+
+    travel_to_and_fro_amt = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
+    local_travel_amt = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
+    food_amt = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
+    event_ticket_amt = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
+    any_extra_amt = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
+
+
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+    event = models.ForeignKey(EventDetails)
+    expense = models.ForeignKey(ExpenseDetails)
+    total_amount_claimed = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
+
+    def __unicode__(self):
+        return self.user
+
--- expensify/settings.py
+++ expensify/settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -119,7 +108,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'exapp',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
