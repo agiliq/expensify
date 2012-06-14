de3e208f93d30213ff1af9b8b99b8d191d6a736f
bitaVersion <pure.vintage.skt@gmail.com>
2012-06-14 17:03:52 +0530

    added user authentication, login and logout views and forms

--- exapp/views.py
+++ exapp/views.py
@@ -2,9 +2,32 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from forms import ExpenseCreationForm, CategoryCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 
+def aclogout(request):
 
-#TODO login and login_required
+    logout(request)
+    return redirect('/')
+
+def aclogin(request):
+    redirect_to = request.REQUEST.get('next', '/')
+    form = AuthenticationForm(data=request.POST or None)
+    if request.POST and form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect(redirect_to)
+            else:
+                return HttpResponse('disabled account')
+        else:
+            return HttpResponse('invalid login')
+    else:
+        return render(request, 'index.html', {'form': form})
+
 
 def claims(request):
     
@@ -14,6 +37,7 @@ def claims(request):
 def index(request):
     
     return render(request, 'index.html', {})
+
 
 def create(request):
     
