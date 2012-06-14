bc4920a81f6e467af61517996119fe83ac251280
bitaVersion <pure.vintage.skt@gmail.com>
2012-06-14 15:23:03 +0530

    changed the model structure

--- exapp/models.py
+++ exapp/models.py
@@ -1,39 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class EventDetails(models.Model):
+class ExpenseCategory(models.Model):
     
-    title = models.CharField(max_length=500)
+    title = models.CharField(max_length=200)
     description = models.TextField()
-    venue = models.CharField(max_length=500)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    date = models.DateField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10,
+            decimal_places=0, default=0)
     
     def __unicode__(self):
-        return self.name
+        return self.title
     
-
-class ExpenseDetails(models.Model):
-    
-
-    travel_to_and_fro_amt = models.DecimalField(max_digits=10,
-            decimal_places=0, default=0)
-    local_travel_amt = models.DecimalField(max_digits=10,
-            decimal_places=0, default=0)
-    food_amt = models.DecimalField(max_digits=10,
-            decimal_places=0, default=0)
-    event_ticket_amt = models.DecimalField(max_digits=10,
-            decimal_places=0, default=0)
-    any_extra_amt = models.DecimalField(max_digits=10,
-            decimal_places=0, default=0)
-
-
 
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User)
-    event = models.ForeignKey(EventDetails)
-    expense = models.ForeignKey(ExpenseDetails)
+    expense = models.ForeignKey(ExpenseCategory)
     total_amount_claimed = models.DecimalField(max_digits=10,
             decimal_places=0, default=0)
 
