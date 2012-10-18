eb68b448b07088da9a90a8dae1913a77e748e060
Shabda Raaj <shabda@agiliq.com>
2012-10-18 15:14:22 +0530

    Added basic tests for index and profile.

--- exapp/tests.py
+++ exapp/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SimpleViews(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.create_user(username="demo", password="demo")
+        self.c.login()
+
+    def test_index(self):
+        "Index reponds correctly to both logged in and logged out users"
+        response = self.c.get("/",)
+        self.assertEqual(response.status_code, 200)
+        self.c.login(username="demo", password="demo")
+        response = self.c.get("/",)
+        self.assertEqual(response.status_code, 200)        
+
+    def test_profile(self):
+        "Profile should redirect for not logged in users and let in for logged in."
+        response = self.c.get("/profile/",)
+        self.assertEqual(response.status_code, 302)        
+        self.c.login(username="demo", password="demo")
+        response = self.c.get("/profile/",)
+        self.assertEqual(response.status_code, 200)        
