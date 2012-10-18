b49593d8131604735e610f97cc2a4c380250958e
Shabda Raaj <shabda@agiliq.com>
2012-10-18 15:16:29 +0530

    Added auth to profile page

--- exapp/views.py
+++ exapp/views.py
@@ -1,12 +1,12 @@
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
-from forms import ExpenseCreationForm, CategoryCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 from models import ExpenseCategory, Expense
+from forms import ExpenseCreationForm, CategoryCreationForm
 
 def oidlogout(request):
 
@@ -24,6 +24,7 @@ def index(request):
     
     return render(request, 'index.html', {})
 
+@login_required
 def profile(request):
 
     e = Expense.objects.filter(usr=request.user)
