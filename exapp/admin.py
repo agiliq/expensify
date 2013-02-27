a1b63e136a892f1357956cc80b70552ec9a3e8cd
Bhaskar <bhaskar8088@gmail.com>
2013-02-27 20:41:48 +0530

    Added ModelAdmin for the model UserProfile

--- exapp/admin.py
+++ exapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import ExpenseCategory, Expense
+from models import ExpenseCategory, Expense, UserProfile
 
 
 class ExpenseAdmin(admin.ModelAdmin):
@@ -12,7 +12,11 @@ class ExpenseCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'max_limit')
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'max_reimbursment')
+
 admin.site.register(ExpenseCategory, ExpenseCategoryAdmin)
 admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
 
 
