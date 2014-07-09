526cf5203e4a1b0aaa419df833de0705fa6f41d3
manjunath24 <hugar.manjunathrt@gmail.com>
2014-07-09 14:52:46 +0530

    import settings

--- exapp/models.py
+++ exapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.core.mail import send_mail
+from django.conf import settings
 
 
 class ExpenseCategory(models.Model):
